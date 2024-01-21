1c76bdbc2851d5145a3e18a9839e21f7d509d213
NT Twan <thedeadstar9@gmail.com>
2024-01-21 19:12:51 +0700

    commit_display_TT_ca_nhan

---
+++ displayInformation.py
@@ -0,0 +1,14 @@
+
+
+# Thông tin cá nhân
+ho_ten = "Nguyễn Thế Toàn"
+tuoi = 23
+gioi_tinh = "Nam"
+dia_chi = "Hà Nội, Việt Nam"
+
+# In thông tin cá nhân
+print("Thông tin cá nhân:")
+print("Họ và tên:", ho_ten)
+print("Tuổi:", tuoi)
+print("Giới tính:", gioi_tinh)
+print("Địa chỉ:", dia_chi)
